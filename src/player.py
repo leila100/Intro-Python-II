11751af7da37d436330e9267ba74c732a74fcb02
Leila <nb.leila10@gmail.com>
2019-08-13 17:55:41 -0500

    Create Player class and create player1 located outside the cave

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, current_position):
+        self.name = name
+        self.current_position = current_position
+
+    def __str__(self):
+        return f"{self.name} is in room {self.current_position.name}"
